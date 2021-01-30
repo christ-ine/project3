6bc7aa03bef9ee79c758b1ca742e3d6f327af6e7
Christine Ross <christinejoyross@gmail.com>
2021-01-29 20:56:01 -0800

    refactoring with redux. navbar login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,19 @@
-import React from 'react'
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomePage from './pages/Homepage/HomePage'
-import QuestionPage from './pages/QuestionPage'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
-// import { Login } from './components/login/login'
-// import { Register } from './components/login/register'
-// import LoginPage from './components/loginpage'
-import MyProfile from './pages/MyProfile/index.js'
-import TopicPage from './pages/TopicPage'
-import SearchResults from './pages/SearchResults'
-import CurrentProfile from './pages/CurrentProfile/CurrentProfile'
+import LoginPage from './pages/LoginPage'
 
+import Header from './components/Header/Header'
 
-
-
-const App = () => {
-
-
+function App() {
   return (
-    <div className='app__main'>
+    <div>
       <Router>
-        
-          <Header />
-
-
-          <main>
-            <Route exact path="/" component={HomePage} />
-            <Container>
-
-              <Route exact path="/question/:currentQuestionId" component={QuestionPage} />
-              <Route path="/profile/:profileId" component={MyProfile} exact/>
-              <Route path="/myprofile" component={CurrentProfile} exact/>
-              <Route path="/topic/:topic" component={TopicPage} exact/>
-              <Route path="/searchresults" component={SearchResults} exact/>
-              <Route path="/login" component={Login} exact/>
-              <Route path="/register" component={Register} exact/>
-
-
-
-              {/* <Route path='/login' component={LoginPage} exact /> */}
-              {/* <Route path='/login' component={Register} exact />
-              <Route path='/login' component={Login} exact /> */}
-
-
-            </Container>
-          </main>
-          <Footer />
-       
-
+        <Header />
+        <Route path="/login" component={LoginPage} exact/>
       </Router>
+
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- client/src/components/Search/SearchElements.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { FaSearch } from 'react-icons/fa'
-
-export const SearchButton = styled(FaSearch)`
-    padding: 0px;
-    font-size: 20px;
-    color: white;
-`
